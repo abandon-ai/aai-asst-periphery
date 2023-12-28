a66b7e96f5d20602dbafc5f3f1ec1180d19bf054
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-28 23:17:12 +0800

    update

--- src/tools/telegram/index.ts
+++ src/tools/telegram/index.ts
@@ -1,22 +1,13 @@
 import {sendMessage, sendMessageHandler} from "./sendMessage";
 // @ts-ignore
 import {RunCreateParams, Runs, RunSubmitToolOutputsParams} from "openai/resources/beta/threads";
-import {sendDice, sendDiceHandler} from "./sendDice";
-import {sendSticker, sendStickerHandler} from "./sendSticker";
-import {forwardMessage, forwardMessageHandler} from "./forwardMessage";
 
 export const TelegramFunctions: Array<RunCreateParams.AssistantToolsFunction> = [
   sendMessage,
-  sendDice,
-  sendSticker,
-  forwardMessage,
 ]
 
 export const functionHandlerMap: {
   [key: string]: (toolCall: Runs.RequiredActionFunctionToolCall, assistant: string) => Promise<RunSubmitToolOutputsParams.ToolOutput>
 } = {
   sendMessage: sendMessageHandler,
-  sendDice: sendDiceHandler,
-  sendSticker: sendStickerHandler,
-  forwardMessage: forwardMessageHandler,
 }
